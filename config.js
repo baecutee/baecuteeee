9e060f2269254892506f69a4cf6f774b55cf429b
baecutee <93479342+baecutee@users.noreply.github.com>
2021-10-31 21:36:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,8 +11,8 @@ const CONFIG = {
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho a<33',
     reply: 'iuaaa<3',
-    mess: 'A bt maaa 😙 . iuemmm nhiều 😘',
-    messDesc: 'Set rela nhoa:33',
+    mess: 'Set rela nhoa:33',
+    messDesc: 'iubae<33',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://m.me/khangthieulang' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
