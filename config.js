4636da48939d4fd24a9f1c7c9d844465f12b305a
baecutee <93479342+baecutee@users.noreply.github.com>
2021-10-31 21:58:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay Thế này đc chưa :)))',
-    btnYes: 'Vẫn cứ là thích a <33',
+    btnYes: 'cứ bị iua ýyyy:3',
     btnNo: 'Không, A à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho a<33',
